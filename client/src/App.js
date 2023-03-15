409b838be9545b5568358190ec6a768d4e9861a4
Zani <zanit@me.com>
2023-03-15 16:23:13 -0400

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,188 @@
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+
+function App() {
+  const [students, setStudents] = useState([]);
+  const [selectedStudentId, setSelectedStudentId] = useState(null);
+  const [selectedStudentName, setSelectedStudentName] = useState("");
+  const [selectedStudentGrade, setSelectedStudentGrade] = useState("");
+  const [selectedStudentProgram, setSelectedStudentProgram] = useState("");
+
+  const formRef = useRef();
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/api/v1/students");
+      setStudents(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const addStudent = async (e) => {
+    e.preventDefault();
+
+    const name = formRef.current.elements.name.value;
+    const program = formRef.current.elements.program.value;
+    const grade = formRef.current.elements.grade.value;
+
+    const newStudent = {
+      name,
+      program,
+      grade,
+    };
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/v1/students",
+        newStudent
+      );
+      setStudents([...students, response.data]);
+      formRef.current.reset();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const selectStudent = (id) => {
+    setSelectedStudentId(id);
+    const selectedStudent = students.find((student) => student.id === id);
+    setSelectedStudentName(selectedStudent.name);
+    setSelectedStudentGrade(selectedStudent.grade);
+    setSelectedStudentProgram(selectedStudent.program);
+  };
+
+  const editStudent = async (e) => {
+    e.preventDefault();
+
+    const name = formRef.current.elements.name.value;
+    const program = formRef.current.elements.program.value;
+    const grade = formRef.current.elements.grade.value;
+
+    const updatedStudent = {
+      id: selectedStudentId,
+      name,
+      program,
+      grade,
+    };
+    try {
+      await axios.put(
+        `http://localhost:8080/api/v1/students/${selectedStudentId}`,
+        updatedStudent
+      );
+      const newStudents = students.map((student) => {
+        if (student.id === selectedStudentId) {
+          return updatedStudent;
+        }
+        return student;
+      });
+      setStudents(newStudents);
+      setSelectedStudentId(null); // Reset selected student ID
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteStudent = async (id) => {
+    try {
+      await axios.delete(
+        `http://localhost:8080/api/v1/students/${selectedStudentId}`
+      );
+
+      setStudents(students.filter((student) => student.id !== id));
+      // setSelectedStudentId(null); // Reset selected student ID
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const renderedStudents = students.map((student) => (
+    <li
+      key={student.id}
+      className={`list-group-item ${
+        selectedStudentId === student.id ? "selected" : ""
+      }`}
+      // className={`list-group-item ${
+      //   selectedStudentId === student.id ? "active" : ""
+      // }`}
+      onClick={() => selectStudent(student.id)}
+    >
+      <div className="student-grouped">
+        <div className="student-name">{`${student.name}`}</div>
+        <div>{` ${student.program}, ${student.grade}`}</div>
+      </div>
+      <div className="button-grouped"></div>
+    </li>
+  ));
+
+  return (
+    <div className="container">
+      <h1 className="text-center">Student Grades</h1>
+      <div className="row">
+        <div className="col-second">
+          <h2>Students</h2>
+          <ul className="list-group">{renderedStudents}</ul>
+        </div>
+
+        <div className="col-first">
+          <h2>Add Student</h2>
+          <form className="form" onSubmit={addStudent} ref={formRef}>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Enter Student Name"
+                value={`${selectedStudentName || ""}`}
+                className="form-control"
+                onChange={(e) => setSelectedStudentName(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="program">Program</label>
+              <input
+                type="text"
+                id="program"
+                placeholder="Enter Program"
+                value={`${selectedStudentProgram || ""}`}
+                className="form-control"
+                onChange={(e) => setSelectedStudentProgram(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="">Grade</label>
+              <input
+                type="text"
+                id="grade"
+                placeholder="Enter Grade"
+                value={`${selectedStudentGrade || ""}`}
+                className="form-control"
+                onChange={(e) => setSelectedStudentGrade(e.target.value)}
+              />
+            </div>
+            <button className="btn btn-primary-submit">Submit</button>
+            {selectedStudentId && (
+              <button className="btn btn-primary-update" onClick={editStudent}>
+                Update
+              </button>
+            )}{" "}
+                <button
+            className="btn btn-primary-update"
+            onClick={(e) => {
+              e.preventDefault(); // Prevent form submission
+              deleteStudent(selectedStudentId);
+            }}
+          >
+            Delete
+          </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || process.argv[2] || 8080;
+const { v4: uuidv4 } = require('uuid');
+const cors = require("cors");
+const dataPath = "./data.json";
+const fs = require('fs');
+
+app.use(
+  cors({
+    origin: ["http://localhost:3002", "http://localhost:8080"],
+  })
+);
+app.use(express.json());
+
+
+if (!fs.existsSync(dataPath)) {
+  fs.writeFileSync(dataPath, JSON.stringify([]));
+}
+let students = JSON.parse(fs.readFileSync(dataPath));
+
+
+
+app.get('/api/v1/students', cors(), (req, res) => {
+  res.json(students);
+  console.log(students);
+});
+
+app.post('/api/v1/students', cors(), (req, res) => {
+  const { name, program, grade } = req.body;
+  const newStudent = {
+    id: uuidv4(),
+    name,
+    program,
+    grade,
+  };
+  console.log(students);
+  students.push(newStudent);
+  res.json(newStudent);
+  fs.writeFileSync(dataPath, JSON.stringify(students));
+});
+
+app.delete('/api/v1/students/:id', cors(), (req, res) => {
+  const id = req.params.id;
+  const deletedStudent = students.find((student) => student.id === id);
+  students = students.filter((student) => student.id !== id);
+  fs.writeFileSync(dataPath, JSON.stringify(students));
+  res.json(deletedStudent);
+});
+
+app.put('/api/v1/students/:id', cors(), (req, res) => {
+  const id = req.params.id;
+  const { name, program, grade } = req.body;
+  const updatedStudent = students.find((student) => student.id === id);
+  updatedStudent.name = name;
+  updatedStudent.program = program;
+  updatedStudent.grade = grade;
+  fs.writeFileSync(dataPath, JSON.stringify(students));
+  res.json(updatedStudent);
+});
+
+app.listen(port, () => console.log(`Listening on ${port}`));
